771f971975fabf744bbaa3a243b00d4c0636a4e4
Luis Theml <44423391+Theml@users.noreply.github.com>
2024-08-19 11:48:33 -0300

    Update Tarefa2.js

--- Tarefa2.js
+++ Tarefa2.js
@@ -1,10 +1,11 @@
 // ### **Tarefa 2: Função Assíncrona**
 
-// Implemente uma função assíncrona chamada `fetchUserData` que faz uma chamada para uma API REST fictícia (simule a chamada usando `fetch` ou `axios`).
-// 1. A função deve fazer uma requisição `GET` para a URL `https://jsonplaceholder.typicode.com/users`.
-// 2. Após obter os dados, filtre os usuários cujo `username` comece com a letra "C".
-// 3. Retorne uma promessa que resolve com esses dados filtrados.
-// **Dica:** Use `async/await` para simplificar o código assíncrono
+/* Implemente uma função assíncrona chamada `fetchUserData` que faz uma chamada para uma API REST fictícia (simule a chamada usando `fetch` ou `axios`).
+1. A função deve fazer uma requisição `GET` para a URL `https://jsonplaceholder.typicode.com/users`.
+2. Após obter os dados, filtre os usuários cujo `username` comece com a letra "C".
+3. Retorne uma promessa que resolve com esses dados filtrados.
+**Dica:** Use `async/await` para simplificar o código assíncrono
+*/
 
 async function fetchUserData() {
     try {
